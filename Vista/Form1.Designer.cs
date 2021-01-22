657e98b42ff94b77b60ec1ad7c28b0ae7ed987cd
Juan Diego Mejía Maestre <Mejiamaestrejuandiego@gmail.com>
2021-01-22 11:15:11 -0500

    Iniciar proyecto

---
+++ Vista/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Vista
+{
+    partial class FormConfiguracion
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormConfiguracion));
+            this.txtMensaje = new System.Windows.Forms.TextBox();
+            this.cbActivo = new System.Windows.Forms.CheckBox();
+            this.listaNumero = new System.Windows.Forms.NumericUpDown();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnRestablecer = new System.Windows.Forms.Button();
+            this.btnCargar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.listaNumero)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtMensaje
+            // 
+            this.txtMensaje.Location = new System.Drawing.Point(12, 12);
+            this.txtMensaje.Multiline = true;
+            this.txtMensaje.Name = "txtMensaje";
+            this.txtMensaje.Size = new System.Drawing.Size(581, 221);
+            this.txtMensaje.TabIndex = 0;
+            // 
+            // cbActivo
+            // 
+            this.cbActivo.AutoSize = true;
+            this.cbActivo.Location = new System.Drawing.Point(513, 250);
+            this.cbActivo.Name = "cbActivo";
+            this.cbActivo.Size = new System.Drawing.Size(56, 17);
+            this.cbActivo.TabIndex = 1;
+            this.cbActivo.Text = "Activo";
+            this.cbActivo.UseVisualStyleBackColor = true;
+            // 
+            // listaNumero
+            // 
+            this.listaNumero.Location = new System.Drawing.Point(369, 249);
+            this.listaNumero.Name = "listaNumero";
+            this.listaNumero.Size = new System.Drawing.Size(120, 20);
+            this.listaNumero.TabIndex = 2;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(12, 250);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 3;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnRestablecer
+            // 
+            this.btnRestablecer.Location = new System.Drawing.Point(93, 250);
+            this.btnRestablecer.Name = "btnRestablecer";
+            this.btnRestablecer.Size = new System.Drawing.Size(75, 23);
+            this.btnRestablecer.TabIndex = 4;
+            this.btnRestablecer.Text = "Restablecer";
+            this.btnRestablecer.UseVisualStyleBackColor = true;
+            this.btnRestablecer.Click += new System.EventHandler(this.btnRestablecer_Click);
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(174, 250);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(75, 23);
+            this.btnCargar.TabIndex = 5;
+            this.btnCargar.Text = "Cargar";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // FormConfiguracion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 281);
+            this.Controls.Add(this.btnCargar);
+            this.Controls.Add(this.btnRestablecer);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.listaNumero);
+            this.Controls.Add(this.cbActivo);
+            this.Controls.Add(this.txtMensaje);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(620, 320);
+            this.Name = "FormConfiguracion";
+            this.Text = "Configuración";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormConfiguracion_FormClosed);
+            this.Load += new System.EventHandler(this.FormConfiguracion_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.listaNumero)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMensaje;
+        private System.Windows.Forms.CheckBox cbActivo;
+        private System.Windows.Forms.NumericUpDown listaNumero;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnRestablecer;
+        private System.Windows.Forms.Button btnCargar;
+    }
+}
+
---
+++ Vista/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Vista.Properties;
+
+namespace Vista
+{
+    public partial class FormConfiguracion : Form
+    {
+        public FormConfiguracion()
+        {
+            InitializeComponent();
+        }
+
+        private void CargarConfiguracion()
+        {
+            this.Location = Settings.Default.Posicion;
+            this.Size = Settings.Default.Tamaño;
+            txtMensaje.Text = Settings.Default.Mensaje;
+            listaNumero.Value = Settings.Default.Numero;
+            cbActivo.Checked = Settings.Default.Activo;
+
+        }
+
+        private void RestablecerConfiguracion()
+        {
+            Settings.Default.Reset();
+            CargarConfiguracion();
+            MessageBox.Show("Se ha restablecido correctamente");
+        }
+
+        private void GuardarConfiguracion()
+        {
+            Settings.Default.Mensaje = txtMensaje.Text;
+            Settings.Default.Numero = (int)listaNumero.Value;
+            Settings.Default.Activo = cbActivo.Checked;
+            Settings.Default.Posicion = this.Location;
+            Settings.Default.Tamaño = this.Size;
+            Settings.Default.Save();
+            MessageBox.Show("Se ha Guardado correctamente");
+
+        }
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            CargarConfiguracion();
+        }
+
+        private void FormConfiguracion_Load(object sender, EventArgs e)
+        {
+            CargarConfiguracion();
+        }
+
+        private void btnRestablecer_Click(object sender, EventArgs e)
+        {
+            RestablecerConfiguracion();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            GuardarConfiguracion();
+        }
+
+        private void FormConfiguracion_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Settings.Default.Mensaje = txtMensaje.Text;
+            Settings.Default.Numero = (int)listaNumero.Value;
+            Settings.Default.Activo = cbActivo.Checked;
+            Settings.Default.Posicion = this.Location;
+            Settings.Default.Tamaño = this.Size;
+            Settings.Default.Save();
+        }
+    }
+}
